bd7eeb48445d401b2ecbb49cacc0692ae09b1925
Jack Aakash <jackaakash4@gmail.com>
2024-11-01 16:11:07 +0545

    Feat: Started a waste-food-management web app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Food Waste Management</h1>
+      <p>This app is used to manange food waste</p>
+    </>
+  )
+}
+
+export default App
